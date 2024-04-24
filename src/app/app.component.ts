7a482bf5e9ad0b3c4f3f0d73fe087a00748a782a
Michael Glombik <michael.glombik@netempire.software>
2024-04-24 17:32:10 +0200

    first implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kraftwertrechner';
+  fgBasisEingabe!: FormGroup;
+  mapProzentualeGewichte!: Map<number, number>;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.fgBasisEingabe = this.formBuilder.group({
+      gewicht: new FormControl<number>(0),
+      wiederholungen: new FormControl<number>(0),
+    });
+    this.fgBasisEingabe.valueChanges.subscribe(
+      (value: { gewicht: number; wiederholungen: number }) =>
+        this.handleWiederholungenChange(value),
+    );
+    this.mapProzentualeGewichte = this.initMapProzentualeGewichte(
+      this.fgBasisEingabe.controls['gewicht'].value,
+      this.fgBasisEingabe.controls['wiederholungen'].value,
+    );
+  }
+
+  private initMapProzentualeGewichte(
+    gewicht: number,
+    wiederholungen: number,
+  ): Map<number, number> {
+    const result: Map<number, number> = new Map<number, number>();
+    const maximalGewicht: number = this.berechneMaximalGewicht(
+      gewicht,
+      wiederholungen,
+    );
+
+    for (let i: number = 30; i <= 100; i += 10) {
+      result.set(i, this.berechneProzentualesGewicht(i, maximalGewicht));
+    }
+    return result;
+  }
+
+  private berechneMaximalGewicht(gewicht: number, wiederholungen: number) {
+    return gewicht * 2;
+  }
+
+  private berechneProzentualesGewicht(
+    prozentsatz: number,
+    maximalGewicht: number,
+  ) {
+    return (maximalGewicht * prozentsatz) / 100;
+  }
+
+  private handleWiederholungenChange(value: {
+    gewicht: number;
+    wiederholungen: number;
+  }) {
+    this.mapProzentualeGewichte = this.initMapProzentualeGewichte(
+      value.gewicht,
+      value.wiederholungen,
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { NgOptimizedImage } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCard } from '@angular/material/card';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    NgOptimizedImage,
+    MatMenuModule,
+    MatSortModule,
+    MatSlideToggleModule,
+    MatRadioModule,
+    MatGridListModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatListModule,
+    FormsModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatDatepickerModule,
+    MatCard
+  ],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
