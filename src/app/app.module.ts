0adc252b100fc58fece884184255c9f7b77ab525
Michael Glombik <michael.glombik@netempire.software>
2024-04-26 16:50:08 +0000

    added pwa support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,6 +26,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCard } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -55,6 +56,12 @@ import { MatChipsModule } from '@angular/material/chips';
     MatDatepickerModule,
     MatCard,
     MatChipsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
